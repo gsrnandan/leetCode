f54b5cc10b5cfb35e687e5a9906248859d5385f7
gsrnandan <raghunandang.89@gmail.com>
2024-09-20 23:27:33 -0400

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -5,3 +5,16 @@ class Solution:
             snum = nums[:i] + ['x'] + nums[i+1:]
             if target - nums[i] in snum:
                 return [i, snum.index(target-nums[i])]
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        length = len(nums)
+        hm = {}
+        for i in range(len(nums)):
+            val = target - nums[i]
+            if val in hm:
+                return [i, hm[val]]
+            hm[nums[i]] = i
+        return []
+                
